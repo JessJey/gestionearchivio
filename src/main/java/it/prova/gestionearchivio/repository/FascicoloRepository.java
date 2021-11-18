5d6482961926fcf56e7a806fe897321fdc6f39db
leoIapp <leoiappelli01@gmail.com>
2021-11-18 09:58:02 +0100

    prova push jess

--- src/main/java/it/prova/gestionearchivio/repository/FascicoloRepository.java
+++ src/main/java/it/prova/gestionearchivio/repository/FascicoloRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import it.prova.gestionearchivio.model.Fascicolo;
 
 public interface FascicoloRepository extends CrudRepository<Fascicolo, Long>, CustomFascicoloRepository{
-
+	
 }
